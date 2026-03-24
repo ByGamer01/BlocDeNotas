79b1ad6a93b074a7cc8c1dae568a92d599927612
ByGamer01 <davidgilrosa1@gmail.com>
2026-03-24 16:32:57 +0100

    f

--- prouex02/src/main/java/es/cide/programacio/MainPanel.java
+++ prouex02/src/main/java/es/cide/programacio/MainPanel.java
@@ -1,13 +1,19 @@
 package es.cide.programacio;
 
 import java.awt.BorderLayout;
+import java.io.File;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class MainPanel extends JPanel {
+    private File fitxerActual; // Nuestro bloque de TextArea debe de ser un objeto de tipo File, para que toda
+                               // la logica funcione
+    private JTextArea texto;
 
     public MainPanel() {
 
@@ -28,9 +34,34 @@ public class MainPanel extends JPanel {
         add(panelBotones, BorderLayout.NORTH);
 
         // Panel central (zona de texto)
-        JPanel seccionTexto = new JPanel();
-        seccionTexto.setBorder(BorderFactory.createTitledBorder("Editor"));
-        add(seccionTexto, BorderLayout.CENTER);
+        texto = new JTextArea();
+        texto.setLineWrap(true);
+        texto.setWrapStyleWord(true);
+
+        // El Text Area siempre debe de ir dentro de un JScrollPane, para que se pueda
+        // scrollear.
+        JScrollPane scroll = new JScrollPane(texto);
+        scroll.setBorder(BorderFactory.createTitledBorder("Editor"));
+
+        add(scroll, BorderLayout.CENTER);
+
+    }
+
+
+    // Metodos de los Botones 
+    private void obrir() {
+
+    }
+
+    private void desar() {
+
+    }
+
+    private void desarCom() {
+
+    }
+
+    private void nou() {
 
     }
 
@@ -38,8 +69,11 @@ public class MainPanel extends JPanel {
         JFrame frame = new JFrame("BlocDeNotas");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
+        frame.setFocusable(false);
+        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.add(new MainPanel());
+
         frame.setVisible(true);
     }
 }
